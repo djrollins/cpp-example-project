59f15c7404c4d16b4e7d7b78ce3ede845e177c8e
Daniel J. Rollins <daniel@djrollins.com>
2018-01-28 19:05:45 +0000

    only add certain compile options if they exist

--- cmake/compile_flags.cmake
+++ cmake/compile_flags.cmake
@@ -1,6 +1,23 @@
 # This module provides functions that add compiler flags to a target based on
 # the detected compiler vendor.
 
+include(CheckCXXCompilerFlag)
+
+function(target_add_compile_options_checked target)
+	message(STATUS "\nARGN: ${ARGN}\n")
+	foreach(flag IN LISTS ARGN)
+		message(STATUS "checking ${flag}")
+		set(has_flag "${target}_has_${flag}")
+		check_cxx_compiler_flag(${flag} ${has_flag})
+		message(STATUS "has_flag: ${has_flag}")
+		if(${has_flag})
+			target_compile_options(${target} PRIVATE ${flag})
+		else()
+			message(WARNING "skipping flag ${flag}")
+		endif()
+	endforeach()
+endfunction()
+
 # compiler flags common to gcc and clang
 function(target_add_common_nix_compile_flags target)
 	message(STATUS "adding *nix flags for ${target}")
@@ -16,9 +33,12 @@ function(target_add_common_nix_compile_flags target)
 			-Wcast-align
 			-Wnon-virtual-dtor
 			-Woverloaded-virtual
-			-Wsequence-point
-			-Wnull-dereference
-			-Wshift-negative-value)
+			-Wsequence-point)
+
+	target_add_compile_options_checked(${target}
+		-Wshift-negative-value
+		-Wnull-dereference
+		-doot)
 endfunction()
 
 function(target_add_clang_compile_flags target)
@@ -28,8 +48,7 @@ endfunction()
 
 function(target_add_gcc_compile_flags target)
 	message(STATUS "adding gcc-specific flags for ${target}")
-	target_compile_options(${target}
-		PRIVATE
+	target_add_compile_options_checked(${target}
 			-Wmisleading-indentation
 			-Wtautological-compare
 			-Wlogical-op
